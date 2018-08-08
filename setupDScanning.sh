b21f5e513a5bdf61b38463b4c0eefbf9fcb5b005
Nixon <nixonrajnirmalraj@ieiworld.com>
2018-08-08 11:39:54 +0800

    updated

--- setupDScanning.sh
+++ setupDScanning.sh
@@ -9,6 +9,7 @@ chmod 777 -R $servicePath
 cp ./${serviceName}@.service /etc/systemd/system/
 chmod 777 /etc/systemd/system/${serviceName}@.service
 
+# install the dependencies for DScanning to run
 #pip3 install -r requirements.txt
 
 systemctl daemon-reload
